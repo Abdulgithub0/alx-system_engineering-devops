fde2f7d8a80d488b447d9696a5f036794516e630
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-06 21:26:54 +0100

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,28 @@
 # Configure Nginx so that its HTTP response contains a custom header on web-01 and web-02 servers
 
 	exec {'update system':
-		provider => 'shell';
-		command  => 'sudo apt-get update';
+		provider => 'shell',
+		command  => 'sudo apt-get update',
 	} ->
 	
 	package {'nginx':
-		provider => 'apt';
-		ensure => 'installed';
+		provider => 'apt',
+		ensure => 'installed',
 	} ->
 	
 	service {'nginx':
-		ensure => 'running';
-		enable => 'true';
+		ensure => 'running',
+		enable => 'true',
 	} ->
 
 	file_line {'custom header':
-		path   => '/etc/nginx/sites-available/default';
-		ensure => 'present';
-		after  => 'listen 80 default_server;';
-		line   => 'add_header X-Served-By $hostname;';
+		path   => '/etc/nginx/sites-available/default',
+		ensure => 'present',
+		after  => 'listen 80 default_server;',
+		line   => 'add_header X-Served-By $hostname;',
 	} ->
 	
 	exec {'restart nginx':
-		provider  => 'shell';
-		command => 'sudo service nginx restart';
+		provider  => 'shell',
+		command => 'sudo service nginx restart',
 	}
