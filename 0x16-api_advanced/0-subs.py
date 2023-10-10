0ee371b02c7e3da1206be07f42d8a7b0f7d1f4d9
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-10-10 15:18:42 +0100

    querying reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """a function that queries the Reddit API
-    and returns the number of subscribers 
+    and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     If an invalid subreddit is given, the function should return 0.
 """
@@ -11,7 +11,7 @@ import requests
 def number_of_subscribers(subreddit):
     """return the number of total subscribers"""
     url = f"https://reddit.com/r/{subreddit}/about/.json"
-    header= {"User-Agent": "Student.Alx.task:v1.0.0"}
+    header = {"User-Agent": "Student.Alx.task:v1.0.0"}
     resp = requests.get(url, headers=header)
     if resp.status_code == 200:
         try:
