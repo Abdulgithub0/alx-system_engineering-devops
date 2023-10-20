40bc9f8e8d5aaf7ffda02b672a4b49c2ab9dbc1c
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-10-20 21:36:36 +0100

    resolve user holberton open file limit issues

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,10 +2,10 @@
 
 exec { 'increase soft limit':
   path    => '/usr/bin/',
-  command => 'sed -i "/holberton	soft  nofile  4096/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton	soft  nofile  10000/" /etc/security/limits.conf',
 }
 
 exec { 'increase hard limit':
-  command => 'sed -i "/holberton	hard  nofile  8192/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton	hard  nofile  20000/" /etc/security/limits.conf',
   path    => '/usr/bin'
 }
