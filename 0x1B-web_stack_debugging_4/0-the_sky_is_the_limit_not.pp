6f0eec7307a4a25b0427dfc09a3958e9a4e68dc7
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-10-20 21:12:07 +0100

    fix server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,11 @@
 # increase total open file for nginx
 
-exec {'increase open files':
-	command => ' sudo sed -i "s/15/4096/" /etc/default/nginx',
-     }
-     ->
-exec {'restart nginx':
-	command => 'sudo service nginx restart'
-     }
+exec {'/usr/bin/sed':
+  path    => '/usr/bin/',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx'
+}
+
+-> exec { 'restart':
+  path    => '/etc/init.d/',
+  command => 'nginx restart'
+}
